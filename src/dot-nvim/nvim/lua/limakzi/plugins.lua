73d5a25f0f1dcd2c28eab14b6f998ce6dfb5f65a
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2024-03-10 23:19:28 +0100

    feat(nvim): Add dracula colorscheme (#21)

--- src/dot-nvim/nvim/lua/limakzi/plugins.lua
+++ src/dot-nvim/nvim/lua/limakzi/plugins.lua
@@ -1,5 +1,10 @@
 vim.cmd [[packadd packer.nvim]]
 
+-- {{{1 dracula/vim
+vim.cmd[[colorscheme dracula]]
+-- }}}1
+
 return require('packer').startup(function(use)
     use {'wbthomason/packer.nvim'}
+    use {'dracula/vim', as = 'dracula'}
 end)
