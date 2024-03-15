d3524eee9e744f9b8862ef6088381a9529a25c61
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2024-03-15 23:24:46 +0100

    feat(nvim): Add colorizer

--- src/os-linux/dot-nvim/nvim/lua/limakzi/plugins.lua
+++ src/os-linux/dot-nvim/nvim/lua/limakzi/plugins.lua
@@ -1,10 +1,15 @@
 vim.cmd [[packadd packer.nvim]]
 
--- {{{1 dracula/vim
-vim.cmd[[colorscheme dracula]]
+-- {{{1 plugins -- dracula/vim
+vim.cmd [[colorscheme dracula]]
+-- }}}1
+
+-- {{{1 plugins -- norcalli/nvim-colorizer
+require('colorizer').setup()
 -- }}}1
 
 return require('packer').startup(function(use)
     use {'wbthomason/packer.nvim'}
     use {'dracula/vim', as = 'dracula'}
+    use {'norcalli/nvim-colorizer.lua'}
 end)
