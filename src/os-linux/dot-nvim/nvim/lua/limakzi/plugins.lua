81adfd2189dd4a03e755f59bf72776c14b2fb3c2
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2024-03-17 19:06:00 +0100

    fix(nvim): Fix telescope find_git mapping (#58)

--- src/os-linux/dot-nvim/nvim/lua/limakzi/plugins.lua
+++ src/os-linux/dot-nvim/nvim/lua/limakzi/plugins.lua
@@ -37,7 +37,7 @@ require('Comment').setup()
 
 -- {{{1 plugins -- nvim-telescope/telescope.nvim
 local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', telescope_builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
