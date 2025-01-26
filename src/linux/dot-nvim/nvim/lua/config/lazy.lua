2a10361b16636deb17fa74817872b02ee54be27e
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2025-01-26 16:21:30 +0000

    fix(nvim): Reformat files

--- src/linux/dot-nvim/nvim/lua/config/lazy.lua
+++ src/linux/dot-nvim/nvim/lua/config/lazy.lua
@@ -1,4 +1,4 @@
--- Bootstrap lazy.nvim
+-- Bootstrap lazy.nvim {{{1
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -14,22 +14,17 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
+-- }}}1
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- Setup lazy.nvim
+-- Setup lazy.nvim {{{1
 require("lazy").setup({
   spec = {
-    -- import your plugins
     { import = "plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
+  -- Automatically check for plugin updates
   checker = { enabled = true },
 })
+--- }}}1
