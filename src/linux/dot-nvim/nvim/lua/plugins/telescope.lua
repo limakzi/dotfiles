210f90e0391c8673941c825a0d9c05751ad91e28
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2025-05-15 06:36:51 +0200

    feat(nvim): Reformat telescope

--- src/linux/dot-nvim/nvim/lua/plugins/telescope.lua
+++ src/linux/dot-nvim/nvim/lua/plugins/telescope.lua
@@ -1,23 +1,21 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim', 
-        tag = '0.1.8',
+  {
+    'nvim-telescope/telescope.nvim', 
+    tag = '0.1.8',
 
-        dependencies = {
-            { 'nvim-lua/plenary.nvim' },
-            { 'nvim-treesitter/nvim-treesitter' },
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable('make') == 1 }
-        },
-        keys = {
-             { "<leader>ff", function() require("telescope.builtin").find_files() end, desc="Find: Files" },
-        },
-        defaults = {
-            layout_config = {
-                vertical = { width = 0.5 }
-            },
-            mappings = {
-                n = {}
-            }
-        }
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-treesitter/nvim-treesitter' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable('make') == 1 }
+    },
+    keys = {
+      { "<leader>ff", function() require("telescope.builtin").fd() end, desc="Find: Files" },
+    },
+    defaults = {
+      layout_config = {
+          vertical = { width = 0.5 }
+      },
+      mappings = { n = {} }
     }
+  }
 }
