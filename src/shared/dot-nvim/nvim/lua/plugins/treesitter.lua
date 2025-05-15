088ec30e1f8cff295e856a5a83493b954fb197d4
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2025-05-16 01:40:46 +0200

    feat(nvim): Add tree-sitter

--- src/shared/dot-nvim/nvim/lua/plugins/treesitter.lua
+++ src/shared/dot-nvim/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,44 @@
 return {
     {
-        'nvim-treesitter/nvim-treesitter'
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate",
+        opts = function(_, _)
+            vim.filetype.add({
+                extension = { 
+                    g = "gap",
+                    gi = "gap",
+                    gd = "gap",
+                    tst = "gaptst",
+                },
+            })
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "gap",
+                callback = function() vim.o.commentstring = "#%s" end,
+            })
+            end,
+        config = function () 
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                ensure_installed = { 
+                    "c", 
+                    "lua", 
+                    "vim", 
+                    "vimdoc", 
+                    "query", 
+                    "elixir", 
+                    "javascript", 
+                    "gap", 
+                    "gaptst", 
+                    "html" 
+                },
+                sync_install = false,
+                highlight = { 
+                    enable = true 
+                },
+                indent = {
+                    enable = true
+                }
+            })
+            end
     }
 }
-
