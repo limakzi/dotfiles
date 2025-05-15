f03f16f0397225a49671554c111bb54e5b57e7e5
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2025-05-16 00:59:17 +0200

    feat(nvim): Add devicons (#92)

--- src/shared/dot-nvim/nvim/lua/plugins/neogit.lua
+++ src/shared/dot-nvim/nvim/lua/plugins/neogit.lua
@@ -3,8 +3,9 @@ return {
         "timuntersberger/neogit",
         cmd = "Neogit",
         dependencies = {
-            "nvim-lua/plenary.nvim", 
-            "sindrets/diffview.nvim",
+            { "nvim-tree/nvim-web-devicons", opts = {} },
+            { 'nvim-lua/plenary.nvim' },
+            { 'sindrets/diffview.nvim' },
         },
         keys = { 
             { "<leader>gg", "<cmd>Neogit<CR>", mode = { "n" }, desc = "Open Neogit" } 
