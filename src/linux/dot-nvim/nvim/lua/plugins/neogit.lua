5857a44b3b31ba26df274a7cd13aead3dc2ff59a
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2025-05-15 18:37:55 +0200

    feat(nvim): Add diffview

--- src/linux/dot-nvim/nvim/lua/plugins/neogit.lua
+++ src/linux/dot-nvim/nvim/lua/plugins/neogit.lua
@@ -1,7 +1,11 @@
 return {
     {
-        "TimUntersberger/neogit",
+        "timuntersberger/neogit",
         cmd = "Neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", 
+            "sindrets/diffview.nvim",
+        },
         keys = { 
             { "<leader>gg", "<cmd>Neogit<CR>", mode = { "n" }, desc = "Open Neogit" } 
         },
