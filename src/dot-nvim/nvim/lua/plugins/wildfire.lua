2fdb237747a16d8691dbab76a9694f5aa081ae2e
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2025-05-30 23:51:04 +0200

    feat(nvim): Improve telescope

--- src/dot-nvim/nvim/lua/plugins/wildfire.lua
+++ src/dot-nvim/nvim/lua/plugins/wildfire.lua
@@ -2,11 +2,27 @@ return {
     {
         "sustech-data/wildfire.nvim",
         event = "VeryLazy",
-        dependencies = {
-            { "nvim-treesitter/nvim-treesitter" },
-        },
+        dependencies = { { "nvim-treesitter/nvim-treesitter" } },
         config = function()
-            require("wildfire").setup()
+            require("wildfire").setup(
+                {
+                  surrounds = {
+                      { "(", ")" },
+                      { "{", "}" },
+                      { "<", ">" },
+                      { "[", "]" },
+                      { "<", ">" },
+                      { "\'", "\'" },
+                      { "\"", "\"" },
+                  },
+                  keymaps = {
+                      init_selection = "<CR>",
+                      node_incremental = "<CR>",
+                      node_decremental = "<S-CR>",
+                  },
+                  filetype_exclude = { "qf" }, --keymaps will be unset in excluding filetypes
+              }
+            )
         end,
     }
 }
