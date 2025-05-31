580a7568e0a8dc54ccb852abef829eb51d5d17fa
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2025-06-01 00:51:06 +0200

    feat(nvim): Add more stuff

--- src/dot-nvim/nvim/lua/plugins/coq.lua
+++ src/dot-nvim/nvim/lua/plugins/coq.lua
@@ -1,18 +1,17 @@
 return {
     {
-        "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
-        lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+        "neovim/nvim-lspconfig",
+        lazy = false,
         dependencies = {
           { "ms-jpq/coq_nvim", branch = "coq" },
           { "ms-jpq/coq.artifacts", branch = "artifacts" },
-
           { 'ms-jpq/coq.thirdparty', branch = "3p" }
         },
         init = function()
-        vim.g.coq_settings = {
-            ["keymap.pre_select"] = true,
-            auto_start = true,
-        }
+            vim.g.coq_settings = {
+                ["keymap.pre_select"] = true,
+                auto_start = true,
+           }
         end,
         config = function()
         end,
