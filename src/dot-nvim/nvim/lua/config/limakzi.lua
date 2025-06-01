8ef840c7e8f0d60bfe86098f058ddea0d37d1050
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2025-06-01 12:31:31 +0200

    fix(nvim): Fix indentations

--- src/dot-nvim/nvim/lua/config/limakzi.lua
+++ src/dot-nvim/nvim/lua/config/limakzi.lua
@@ -85,9 +85,9 @@ vim.opt.smartindent = true
 
 vim.opt.expandtab = true
 vim.opt.shiftround = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 -- }}}1
 --
 -- {{{1 Spell-checking.
