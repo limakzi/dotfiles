370e08991322017a591d13f0454a2eaa278507fe
Kamil Zabielski <50334623+limakzi@users.noreply.github.com>
2025-06-01 12:12:11 +0200

    feat(nvim): Add gruvbox theme and ufo plugin

--- src/dot-nvim/nvim/lua/plugins/themes-bg.lua
+++ src/dot-nvim/nvim/lua/plugins/themes-bg.lua
@@ -1,20 +1,33 @@
 return {
     {
-        'typicode/bg.nvim',
-        lazy = false
-    },
-    { 
-        "catppuccin/nvim", 
-        name = "catppuccin", 
+        "ellisonleao/gruvbox.nvim",
         priority = 1000,
-        -- Configuration block for the Catppuccin theme
-        -- Default behavior: Uses the "mocha" flavor with no additional customization.
-        -- To customize, modify the options table below.
         config = function()
-            require("catppuccin").setup({
-                flavor = "mocha", -- Available flavors: latte, frappe, macchiato, mocha
-                -- Add other customization options here if needed
-            })
+          require("gruvbox").setup({
+            undercurl = true,
+            underline = true,
+            bold = true,
+            terminal_colors = true,
+            italic = {
+              strings = true,
+              emphasis = true,
+              comments = true,
+              operators = false,
+              folds = true,
+            },
+            strikethrough = true,
+            invert_selection = false,
+            invert_signs = true,
+            invert_tabline = false,
+            inverse = true,
+            contrast = "hard",
+            palette_overrides = {},
+            overrides = {},
+            dim_inactive = false,
+            transparent_mode = true,
+          })
+          vim.o.background = "dark"
+          vim.cmd("colorscheme gruvbox")
         end
     }
 }
